f42554fdcef05918431af8fa255f353e2dd867c8
inotia00 <108592928+inotia00@users.noreply.github.com>
2025-07-13 21:26:59 +0900

    feat(YouTube - Hook download actions): Update the description of the `Override playlist download button` setting

--- extensions/shared/src/main/java/app/revanced/extension/youtube/patches/utils/PatchStatus.java
+++ extensions/shared/src/main/java/app/revanced/extension/youtube/patches/utils/PatchStatus.java
@@ -3,6 +3,7 @@ package app.revanced.extension.youtube.patches.utils;
 import app.revanced.extension.shared.utils.Utils;
 
 public class PatchStatus {
+    public static final String SPOOF_APP_VERSION_TARGET_DEFAULT_VALUE = "18.17.43";
 
     public static boolean ImageSearchButton() {
         // Replace this with true if the Hide image search buttons patch succeeds
@@ -56,7 +57,7 @@ public class PatchStatus {
     }
 
     public static String SpoofAppVersionDefaultString() {
-        return "18.17.43";
+        return SPOOF_APP_VERSION_TARGET_DEFAULT_VALUE;
     }
 
     // Modified by a patch. Do not touch.
