52d13d5562bfe6329426da94d7791db5cf3641c2
Eric Ahn <wchill1337@gmail.com>
2026-01-10 00:50:12 -0800

    fix: Remove usage of fingerprint DSL API that was causing errors with dev versions of Morphe Manager

--- patches/src/main/kotlin/app/morphe/patches/reddit/utils/extension/hooks/ApplicationInitHook.kt
+++ patches/src/main/kotlin/app/morphe/patches/reddit/utils/extension/hooks/ApplicationInitHook.kt
@@ -1,10 +1,13 @@
 package app.morphe.patches.reddit.utils.extension.hooks
 
+import app.morphe.patcher.Fingerprint
 import app.morphe.patches.shared.extension.extensionHook
 
-internal val applicationInitHook = extensionHook {
-    custom { method, _ ->
-        method.definingClass.endsWith("/FrontpageApplication;") &&
-                method.name == "onCreate"
-    }
-}
+internal val applicationInitHook = extensionHook(
+    fingerprint = Fingerprint(
+        custom = { method, _ ->
+            method.definingClass.endsWith("/FrontpageApplication;") &&
+                    method.name == "onCreate"
+        }
+    )
+)
