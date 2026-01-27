4cd4301b9f2cd30b29255faaf89d47cb4486342a
Eric Ahn <wchill1337@gmail.com>
2026-01-26 16:06:49 -0800

    Remove reddit patches

--- patches/src/main/kotlin/app/morphe/patches/reddit/utils/extension/Constants.kt
+++
@@ -1,7 +0,0 @@
-package app.morphe.patches.reddit.utils.extension
-
-@Suppress("MemberVisibilityCanBePrivate")
-internal object Constants {
-    const val EXTENSION_PATH = "Lapp/morphe/extension/reddit"
-    const val PATCHES_PATH = "$EXTENSION_PATH/patches"
-}
